d0083452a8899b6ac3454aa4c10e4afe3d1b9e99
sreeraj <sreerajs728@gmail.com>
2024-02-24 22:04:09 +0530

    home screen started

--- aime_app/lib/screens/Home.dart
+++ aime_app/lib/screens/Home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
@@ -10,34 +10,49 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 51, 72, 80),
-        title: const Text(
-          'HOME',
-          style: TextStyle(
-            color: Color.fromARGB(99, 100, 185, 200),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 51, 72, 80),
+          title: const Text(
+            'HOME',
+            style: TextStyle(
+              color: Color.fromARGB(99, 100, 185, 200),
+            ),
           ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Padding(
+        body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+              Expanded(
+                  child: Padding(
                 padding: EdgeInsets.all(5.0),
                 child: Column(
                   children: [
-                    Container(
-                      color:Colors.red,
-                      
-                    )
+                    Card(
+                        color: Colors.red,
+                        elevation: 10.0,
+                        child: Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(5.0),
+                              child: const CircleAvatar(
+                                backgroundImage:
+                                    AssetImage('assets/images/bg.jpg'),
+                                radius: 50.0,
+                              ),
+                            ),
+                            Container(
+                              child:Center(
+                                child: Text('data'),
+                              ),
+                            )
+                          ],
+                        ))
                   ],
                 ),
-              )
-            )]
-        )));
+              ))
+            ])));
   }
 }
